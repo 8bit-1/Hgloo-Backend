7d2b022df843c8aa2d1afd562cb00bd95c73c258
Jorge-Marin <jorgemarin9915@gmail.com>
2021-03-12 03:51:01 -0600

    It's dont work the last changes, tried again

--- routes/location.js
+++ routes/location.js
@@ -7,7 +7,7 @@ const locationS = require('../services/location');
 //GET Countries
 //localhost:3000/locations/country
 router.get('/country', async function(req, res, next){
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     try {
         res.json( await locationS.getCountry());
     } catch (error) {
@@ -18,7 +18,7 @@ router.get('/country', async function(req, res, next){
 //GET country provinces by country Id
 //localhost:3000/locations/province/3
 router.get('/province/:id', async function(req, res, next){
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     try {
         res.json( await locationS.getProvinceById(req.params.id));
     } catch (error) {
@@ -29,7 +29,7 @@ router.get('/province/:id', async function(req, res, next){
 //GET province cities by province id
 //localhost:3000/locations/city/3
 router.get('/city/:id', async function(req, res, next){
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     try {
         res.json( await locationS.getCityById(req.params.id));
     } catch (error) {
