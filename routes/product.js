26f21cb4c3108a88aea8eb615e04670dc928db72
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-03-19 18:41:18 -0600

    Update product.js
    idUser was added as a parameter to the registerProduct method

--- routes/product.js
+++ routes/product.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const producT = require('../services/product')
 
 //POST
-router.post('/register-product', async function (req,res, next){  
+router.post('/register-product/:idUser', async function (req,res, next){  
     try {
-        res.json( await producT.registerProduct(req.body) );
+        res.json( await producT.registerProduct(req.body,req.params.idUser) );
     } catch (error) {
         console.error(`Error while registering Product`, error.message);
         next(error);
