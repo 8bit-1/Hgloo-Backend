004a670f5413858cb54a06fac5a755a67c8470fa
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-03-23 09:34:55 -0600

    Update product.js
    the currency was added to the product

--- services/product.js
+++ services/product.js
@@ -3,13 +3,14 @@ const db = require('./db');
 async function getProduct(idUser){
     const result = await db.queryP(`SELECT producto.idProducto, producto.Producto,
     ciudad.nombreCiudad, pais.pais,condicion.condicion,
-    producto.costo FROM producto 
+    CONCAT(producto.costo, " ", moneda.Moneda) AS costo FROM producto 
     INNER JOIN ciudad ON producto.idCiudadProducto=ciudad.idCiudad 
     AND  producto.idDepartamentoProducto=ciudad.idDepartamento
     AND producto.idPaisProducto=ciudad.idPais 
     INNER JOIN departamento ON producto.idDepartamentoProducto=departamento.idDepartamento
     INNER JOIN pais ON producto.idPaisProducto=pais.idPais
     INNER JOIN condicion ON producto.idCondicion=condicion.idCondicion
+    INNER JOIN moneda ON producto.idMoneda=moneda.idMoneda
     WHERE  producto.idEstadoProducto<>2
     AND producto.usuario=?
     ORDER BY producto.fechaPublicacion DESC;`,[idUser]);
