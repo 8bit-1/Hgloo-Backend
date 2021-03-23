e08343ddf2240aa15f9a4e46318f56bc97dce031
AlexReyes76 <alex_dyk@outlook.es>
2021-03-23 11:49:24 -0600

    Update categories.js
    Functions were added to manage to modify and add categories, also a function to modify the validity of the product

--- services/categories.js
+++ services/categories.js
@@ -25,7 +25,41 @@ async function deleteCategory(idCategory){
 }
 
 
+async function createCategory(Category){
+        const result = await db.queryP(
+            `INSERT INTO categoria (nombreCategoria) VALUES (?)`,[Category.name]);
+        if (!result) { return [];
+        }
+        
+        let message = 'Error creating category';
+        if (result.affectedRows) {
+            message = 'Category created sucessfully';
+        }
+
+    return message;
+
+}
+
+
+async function updateValidity(Vigencia){
+    const result = await db.queryP(
+        `call modificarVigencia (?,?)`,[Vigencia.product,Vigencia.service]);
+    if (!result) { return [];
+    }
+    
+    let message = 'Error updating validity';
+    if (result.affectedRows) {
+        message = 'Validity updated sucessfully';
+    }
+
+return message;
+
+}
+
+
 module.exports = {
     getCategory,
-    deleteCategory
+    deleteCategory,
+    createCategory,
+    updateValidity
 }
