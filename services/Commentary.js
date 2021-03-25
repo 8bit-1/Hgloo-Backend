e087a867b2ee851d20708a3a7fa158a5afc9a589
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-03-25 09:47:27 -0600

    Update Commentary.js
    Added the function to comment on a product

--- services/Commentary.js
+++ services/Commentary.js
@@ -18,8 +18,27 @@ async function commentUser(comment,commentator,commented){
     return message;
 }
 
+async function commentProduct(comment,commentator,idProduct){
+    const result = await db.queryP(
+        `INSERT INTO comentario (Comentario, Comentador, Comentado,idProductoComentado) VALUES (?, ?, (SELECT usuario FROM producto where idProducto=?), ?)`,
+        [
+            comment.description,
+            commentator,
+            idProduct,
+            idProduct
+        ]
+    );
+
+    let message = 'Error creating commentary';
+
+    if (result.affectedRows) {
+        message = 'commentary sucessfully';
+    }
+    return message;
+}
 
 
 module.exports = {
-    commentUser   
+    commentUser,
+    commentProduct   
 }
