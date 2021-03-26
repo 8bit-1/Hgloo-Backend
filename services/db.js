f266fb3cfbe38761fd0319078bd214de627f250d
8bit-1 <54825061+8bit-1@users.noreply.github.com>
2021-03-25 19:45:13 -0600

    Bug solucionado
    Problema solucionado de maximo numero de conexiones a la base. Usando pool de conexiones.

--- services/db.js
+++ services/db.js
@@ -1,20 +1,32 @@
 const mysql =  require('mysql2/promise');
 const config = require('../config');
 
-async function queryP(sql,params){
-    const connection = await mysql.createConnection(config.db);
-    const [results,] = await connection.execute(sql,params);
+const pool = mysql.createPool(config.db)
 
-    return results
+
+// async function queriP(sql,params){
+//     const connection = await mysql.createConnection(config.db);
+//     const [results,] = await connection.execute(sql,params);
+
+//     return results
+// }
+
+// async function queri(sql){
+//     const connection = await mysql.createConnection(config.db);
+//     const [results,] = await connection.query(sql)
+
+//     return results
+// }
+
+async function queryP(sql, params){
+    const [results,] = await pool.execute(sql, params);
+    return results;
 }
 
 async function query(sql){
-    const connection = await mysql.createConnection(config.db);
-    const [results,] = await connection.query(sql)
-
-    return results
+    const [results,] = await pool.query(sql);
+    return results;
 }
-
 
 
 module.exports = {
