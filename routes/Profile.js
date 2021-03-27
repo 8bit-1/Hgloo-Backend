6151fb30b4f9efa2c199d28563370e2ea629057d
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-03-27 01:03:25 -0600

    Update Profile.js
    add new function mostProfile

--- routes/Profile.js
+++ routes/Profile.js
@@ -17,5 +17,16 @@ router.get('/profile/:token', async function(req, res, next){
     }
 });
 
-
+router.get('/:token', async function(req, res, next){
+    try {
+        admin.auth().verifyIdToken( req.params.token ).then( async ( decodedToken ) => {
+            const uid = decodedToken.uid;
+            const user = await profilE.mostProfile(uid);
+            res.json( user );
+        });
+        
+    } catch (error) {
+        console.error("Error while getting User: ",error)
+    }
+});
 module.exports = router;
