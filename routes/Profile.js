dfc008b2ff1c9d216e24c7f2094bbe71e480c714
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-03-27 02:15:05 -0600

    Update Profile.js
    show perfil aguila to geopardo

--- routes/Profile.js
+++ routes/Profile.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const profilE = require('../services/Profile');
 
-router.get('/profile/:idUser', async function(req, res, next){
+router.get('/showprofile/:idUser', async function(req, res, next){
     try {
         res.json( await profilE.getProfile(req.params.idUser));
     } catch (error) {
         console.error("Error while getting User: ",error)
     }
+    
 } );
 
 
