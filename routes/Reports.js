d0df2174506d5f39ff14e653a23aed0fb5a2c36e
AlexReyes76 <alex_dyk@outlook.es>
2021-03-27 11:24:58 -0600

    Update Reports.js
    functions have been added to obtain user, product and comment reports

--- routes/Reports.js
+++ routes/Reports.js
@@ -4,13 +4,33 @@ const router = express.Router();
 const reportS = require('../services/Reports');
 
 //GET reports
-router.get('/get-reports', async function(req, res, next){
+router.get('/product', async function(req, res, next){
     try {
-        res.json( await reportS.getReport());
+        res.json( await reportS.getReportProduct());
     } catch (error) {
-        console.error("Error while getting reports",error)
+        console.error("Error while getting reports products",error)
     }
 } );
+
+//GET 
+router.get('/user', async function(req, res, next){
+    try {
+        res.json( await reportS.getReportUser());
+    } catch (error) {
+        console.error("Error while getting reports users",error)
+    }
+} );
+
+
+//GET 
+router.get('/comment', async function(req, res, next){
+    try {
+        res.json( await reportS.getReportComment());
+    } catch (error) {
+        console.error("Error while getting reports comments",error)
+    }
+} );
+
 
 
 module.exports = router;
