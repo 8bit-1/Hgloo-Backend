388cb94e2f8ab70b41b83f4289a9b1c67a4cad34
AlexReyes76 <alex_dyk@outlook.es>
2021-04-05 22:17:18 -0600

    Update Complaint.js
    the reporting functions by token was configured

--- routes/Complaint.js
+++ routes/Complaint.js
@@ -4,9 +4,13 @@ const admin = require("firebase-admin");
 const complainT = require('../services/Complaint');
 
 //POST
-router.post('/add-complaint/:whistleblower/:denounced', async function (req,res, next){  
+router.post('/report-user/:token/:denounced', async function (req,res, next){  
     try {
-        res.json( await complainT.addComplaint(req.body,req.params.whistleblower,req.params.denounced) );
+        admin.auth().verifyIdToken( req.params.token ).then( async ( decodedToken ) => {
+            const uid = decodedToken.uid;
+            const user = await complainT.addComplaint(req.body,uid,req.params.denounced);
+            res.json( user );
+        });
     } catch (error) {
         console.error(`Error while creating complaint`, error.message);
         next(error);
@@ -15,24 +19,19 @@ router.post('/add-complaint/:whistleblower/:denounced', async function (req,res,
 
 
 //POST
-router.post('/report-product/:whistleblower/:idProduct', async function (req,res, next){  
+router.post('/report-product/:token/:idProduct', async function (req,res, next){  
     try {
-        res.json( await complainT.reportProduct(req.body,req.params.whistleblower,req.params.idProduct) );
+        admin.auth().verifyIdToken( req.params.token ).then( async ( decodedToken ) => {
+            const uid = decodedToken.uid;
+            const user = await complainT.reportProduct(req.body,uid,req.params.idProduct);
+            res.json( user );
+        });
     } catch (error) {
         console.error(`Error while reporting product`, error.message);
         next(error);
     }
 });
 
-//POST
-router.post('/report-comment/:whistleblower/:commentary', async function (req,res, next){  
-    try {
-        res.json( await complainT.complaintComment(req.body,req.params.whistleblower,req.params.commentary) );
-    } catch (error) {
-        console.error(`Error while reporting comment`, error.message);
-        next(error);
-    }
-});
 
 //POST
 router.post('/report-comments/:token/:commentary', async function (req,res, next){  
