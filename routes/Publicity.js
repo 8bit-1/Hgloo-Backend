84b1acc8099e8c01ab3bc8a25d0ac009e6794862
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-04-09 00:35:42 -0600

    Create Publicity.js
    se agregó la función para obtener los productos publicitados

---
+++ routes/Publicity.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const admin = require('firebase-admin');
+
+const publI = require('../services/Publicity');
+
+//POST
+router.post('/insert/:idProduct/:token', async function (req,res, next){
+    try {
+        admin.auth().verifyIdToken( req.params.token ).then( async ( decodedToken ) => {
+            const uid = decodedToken.uid;
+            const user = await publI.Publicity(uid,req.params.idProduct);
+            res.json(user)});
+    } catch (error) {
+        console.error('error inserting publicity', error.message);
+        next(error);
+    }
+});
+
+
+//GET
+router.get('/getPublicity', async function(req, res, next){
+    try {
+        res.json( await publI.getPublicity());
+    } catch (error) {
+        console.error("Error while getting Genre: ",error)
+    }
+} );
+module.exports = router;
