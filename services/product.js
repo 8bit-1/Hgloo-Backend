01ce9dd4de9108b6852790acaf9f3ace4dac3681
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-04-30 00:36:59 -0600

    Update product.js
    Se agregó que no trajera productos de usuarios demandados en usuarios registrados

--- services/product.js
+++ services/product.js
@@ -181,9 +181,11 @@ async function getAllProductsUserLogged( uid, coin ) {
         INNER JOIN condicion ON producto.idCondicion=condicion.idCondicion
         INNER JOIN moneda ON producto.idMoneda=moneda.idMoneda
         INNER JOIN imagenesurl ON imagenesurl.idProducto=producto.idProducto
+        INNER JOIN usuario ON usuario.idUsuario=producto.usuario
         WHERE  producto.idEstadoProducto<>2
         AND imagenesurl.idProducto=producto.idProducto
         AND producto.usuario <> '${ uid }'
+        AND usuario.idEstado<>2
         GROUP BY producto.idProducto
         ORDER BY datep DESC
         ) SELECT idProduct, usuario, productName, location , state,
